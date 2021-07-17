1715bcaadb31e81e6e3eaec3597a05b0f33a7a56
Rhett Coleman <68975053+RhettColeman@users.noreply.github.com>
2021-07-17 15:50:14 -0400

    v 0.2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const title = 'Welcome to CyberRhett.com'
-  const likes = 50;
-  const link = 'https://www.google.com';
-
-
   return (
     <div className="App">
-     `<div className="content">
-        <h1>{ title }</h1>
-        <p>Liked { likes } times</p>
-        <p> {10} </p>
-        <p> { Math.random() } </p>
-        <a href={link}> Google Link</a>
-
-     </div>
-
+    <Navbar />
+    <div className="content">
+      <Home />
+      </div>
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Nabar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>Cyber Rhett</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">New Blog</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Nabar;
