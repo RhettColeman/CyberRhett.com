21ada81fc5ea2b61aef523b9e90203aa9d371f68
Rhett Coleman <68975053+RhettColeman@users.noreply.github.com>
2021-07-17 16:10:35 -0400

    v 0.3

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,7 @@ const Nabar = () => {
             <h1>Cyber Rhett</h1>
             <div className="links">
                 <a href="/">Home</a>
-                <a href="/create">New Blog</a>
+                <a href="/create"> New Blog</a>
             </div>
         </nav>
      );
